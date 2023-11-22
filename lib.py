baed41af8481ae9e3f60ec47f11505cc8e9ccbdf
Cosmic-rare <ysolakey1116@gmail.com>
2023-11-22 11:20:26 +0900

    完成

--- lib.py
+++ lib.py
@@ -1,4 +1,5 @@
 import json
+from datetime import date, datetime, timedelta
 
 def loadSetting():
     with open("setting.json") as fSetting:
@@ -7,3 +8,29 @@ def loadSetting():
 def loadData():
     with open("data.json") as fData:
         return json.load(fData)
+
+def getFirstDay():
+    d = datetime.strptime(loadData()["start"], '%y/%m/%d')
+    return date(d.year, d.month, d.day)
+
+def joinSubject():
+    d = getFirstDay()
+    subjectSetting = loadSetting()["subjects"]
+    joinedSubject = []
+    for i in loadData()["subjects"]:
+        joined = []
+        for j in i:
+            joined.append(subjectSetting[j])
+        joinedSubject.append({ "date": d, "subjects": joined })
+        d = d + timedelta(days=1)
+    return joinedSubject
+
+def joinTime():
+    joinedSubject = joinSubject()
+    schedule = loadSetting()["schedules"][loadData()["schedule"]]
+    for i in range(len(joinedSubject)):
+        for j in range(len(joinedSubject[i]["subjects"])):
+            joinedSubject[i]["subjects"][j]["start"] = "{}T{}:00".format(joinedSubject[i]["date"].isoformat(), schedule[j][0])
+            joinedSubject[i]["subjects"][j]["end"] = "{}T{}:00".format(joinedSubject[i]["date"].isoformat(), schedule[j][1])
+    return joinedSubject
+
--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-from lib import loadSetting, loadData
+from lib import joinTime
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-print(loadSetting())
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+creds = flow.run_local_server(port=0)
+service = build('calendar', 'v3', credentials=creds)
 
-print(loadData())
+data = joinTime()
+
+for i in data:
+    for j in i["subjects"]:
+        event = {
+            "summary": j["title"],
+            "start": {
+                "dateTime": j["start"],
+                "timeZone": "Japan"
+            },
+            "end": {
+                "dateTime": j["end"],
+                "timeZone": "Japan"
+            },
+            "colorId": j["color"]
+        }
+        created_event = service.events().insert(calendarId='primary', body=event).execute()
